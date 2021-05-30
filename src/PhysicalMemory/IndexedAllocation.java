5b06395158d573145d1371b6ce0e63986959e542
ShawkyDev <60538247+ShawkyDev@users.noreply.github.com>
2021-05-30 17:34:31 +0200

    imp. linked & indexed allocation

--- src/PhysicalMemory/IndexedAllocation.java
+++ src/PhysicalMemory/IndexedAllocation.java
@@ -5,6 +5,13 @@ import java.util.ArrayList;
 public class IndexedAllocation implements AllocatoinStrategy {
     @Override
     public ArrayList<Integer> Allocate(int size) {
+        ArrayList<Integer> allocated = new ArrayList<>();
+        int N = PhysicalMemeoryManger.getSize();
+        var bitVector = PhysicalMemeoryManger.bitVector;
+        for (int i = 0; i < N; ++i)
+            if (!bitVector[i])
+                allocated.add(i);
+        if (allocated.size() >= size + 1) return allocated; // extra space for the index
         return null;
     }
 }
--- src/PhysicalMemory/LinkedAllocation.java
+++ src/PhysicalMemory/LinkedAllocation.java
@@ -5,6 +5,13 @@ import java.util.ArrayList;
 public class LinkedAllocation implements AllocatoinStrategy{
     @Override
     public ArrayList<Integer> Allocate(int size) {
+        ArrayList<Integer> allocated = new ArrayList<>();
+        int N = PhysicalMemeoryManger.getSize();
+        var bitVector = PhysicalMemeoryManger.bitVector;
+        for (int i = 0; i < N; ++i)
+            if (!bitVector[i])
+                allocated.add(i);
+        if(allocated.size()>=size) return allocated;
         return null;
     }
 }
